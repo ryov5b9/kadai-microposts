274a8aee207307bdeaddf6fa1c7d00f8c49a1ae7
ryo.v5b9 <hryov5b9@gamil.com>
2019-05-16 08:34:33 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
 
- include SessionsHelper
- 
+  include SessionsHelper
+
   private
 
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
+  end
+  def counts(user)
+    @count_microposts = user.microposts.count
   end
 end
